578edcf6a510695524ddd467d7505e3979d8a726
Benjamin <benjaminignacio.polanco@gmail.com>
2024-07-13 09:44:19 -0400

    definiendo las clases y cosas

---
+++ BenjaminPolanco.py
@@ -0,0 +1,38 @@
+import random
+import csv
+import math
+
+trabajadores = ["Juan Pérez", "María García", "Carlos López", "Ana Martínez", "Pedro Rodríguez", "Laura Hernández", "Miguel Sánchez", "Isabel Gómez", "Francisco Díaz", "Elena Fernández"]
+sueldos = []
+
+def asignar_sueldo_random(self):
+    
+    
+    
+def clasificar_sueldos(self):
+    
+def ver sueldos():
+
+
+def reporte_de_sueldos():
+    
+def salir():
+
+def menu():
+    while True:
+        print("menu \n1.asignar sueldos aleatorios\n2.clasificar sueldos\n3.ver estadistica\n4.reporte de sueldo\n5.salir")
+        opc= int(input())
+        
+        if opc == 1:
+            asignar_sueldo_random()
+        elif opc == 2:()
+            clasificar_sueldos()
+        elif opc == 3:
+            ver_sueldos()
+        elif opc ==4:
+            reporte_de_sueldos()
+        elif opc == 5:
+            salir()    
+        
+menu()        
+        

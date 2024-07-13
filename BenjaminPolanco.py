634480cabd0589d77f5a82c291cce9452e0afb30
Benjamin <benjaminignacio.polanco@gmail.com>
2024-07-13 10:41:22 -0400

    teno problemas con generar el documento

--- BenjaminPolanco.py
+++ BenjaminPolanco.py
@@ -2,7 +2,7 @@ import random
 import csv
 import math
 
-trabajadores = ["Juan Pérez", "María García", "Carlos López", "Ana Martínez", "Pedro Rodríguez", "Laura Hernández", "Miguel Sánchez", "Isabel Gómez", "Francisco Díaz", "Elena Fernández"]
+trabajadores = ["Alejandro Gómez", "Sofía Jiménez", "Javier Ramírez", "Lucía Castillo", "Daniel Moreno", "Valeria Ortiz", "Andrés Soto", "Camila Vargas", "Emilio Flores", "Natalia Reyes"]
 sueldos = []
 
 def asignar_sueldo_random():
@@ -31,9 +31,23 @@ def ver_sueldos():
     promedio_sueldo=sum(sueldos) / len(sueldos)
     media_geometrica = math.prod(sueldos) **(1/len(sueldos))
 def reporte_de_sueldos():
+    descuentos =[]
+    for sueldo in sueldos:
+        salud = sueldo*0.07
+        afp = sueldo *0.12
+        suel_liquido = sueldo -salud -afp
+        descuentos.append((sueldo,salud, afp, sueldo))
+        
+        with open ('reporte_de_sueldos.csv', 'w', newline='') as csvfile:
+            nombres_doc= ['Nombre', 'Sueldo Base', 'Descuento Salud', 'Descuento AFP', 'Sueldo Líquido']
+            writer = csv.DictWriter(csvfile, nombres_doc=nombres_doc)
+            writer.writeheader()
+            for i, desc in enumerate(descuentos):
+                writer.writerow({'Nombre': trabajadores[i], 'Sueldo Base': desc[0], 'Descuento Salud': desc[1], 'Descuento AFP': desc[2], 'Sueldo Líquido': desc[3]})
+        print("se genero reporte_sueldos.csv")
     
 def salir():
-
+    exit()
 def menu():
     while True:
         print("menu \n1.asignar sueldos aleatorios\n2.clasificar sueldos\n3.ver estadistica\n4.reporte de sueldo\n5.salir")
@@ -49,6 +63,9 @@ def menu():
             reporte_de_sueldos()
         elif opc == 5:
             salir()    
+        else:
+            print("no valido")
+            continue
         
 menu()        
         
